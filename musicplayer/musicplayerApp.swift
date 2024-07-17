8a79ee36d72bdc89e73a6d8ff6c99ef0b6e4ea43
Zomatree <me@zomatree.live>
2024-07-17 02:58:49 +0100

    Initial Commit

---
+++ musicplayer/musicplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicplayerApp.swift
+//  musicplayer
+//
+//  Created by Angelo on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct musicplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
